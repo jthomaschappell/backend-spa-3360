a970dba240bb059a0c22d5b9da9759732217da89
Thomas Chappell <jthomaschappell@gmail.com>
2025-04-22 23:44:23 -0600

    new branch commited

--- config.py
+++ config.py
@@ -6,6 +6,17 @@ class Settings(BaseSettings):
     allow_origins: list[AnyHttpUrl]
     api_gateway_token: str
     
+    # Database settings
+    postgres_user: str = "postgres"
+    postgres_password: str = "postgres"
+    postgres_host: str = "localhost"
+    postgres_port: str = "5432"
+    postgres_db: str = "react_binder_db"
+    
+    @property
+    def database_url(self) -> str:
+        return f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+    
     class Config:
         env_file = ".env"
 
