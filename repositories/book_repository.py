3366afb1ad55a1dcaa2ecde7959c94371b6f6e12
Thomas Chappell <jthomaschappell@gmail.com>
2025-04-27 18:40:14 -0600

    env stuff

--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -16,24 +16,3 @@ class BookRepository:
                 )
             else:
                 raise Exception(f"Failed to fetch book: {response.status_code}")
-
-
-
-
-# from schemas.swapi_schema import FilmResponse
-
-
-# class SWAPIRepository:
-#     @staticmethod
-#     async def get_all_films() -> FilmResponse | None:
-#         url = "https://swapi.dev/api/films/"
-#         async with httpx.AsyncClient() as client:
-#             response = await client.get(url)
-#             if response.status_code == 200:
-#                 return FilmResponse(**response.json())
-#         return None
-
-
-"""
-Instead of taking 
-"""
